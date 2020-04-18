8f4038d6c475531ab7b397c86d7a72fec6280039
korlando7 <korlando7@gmail.com>
2020-04-18 16:46:07 -0400

    cleanup

--- runner.js
+++ runner.js
@@ -1,9 +1,10 @@
 const fs = require('fs').promises
 const cp = require('child_process')
 const ulid = require('ulid')
+const languages = require('./languages')
 
-const buildArgs = (config, name) => {
-  const { image, cmd, extension } = config
+const buildArgs = (config, name, file) => {
+  const { image, cmd } = config
   return [
     'run',
     '--rm',
@@ -15,27 +16,19 @@ const buildArgs = (config, name) => {
     '/usr/src/app',
     image,
     cmd,
-    `${name}${extension}`
+    file
   ]
 }
 
-const parseLanguage = (language) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile('./languages.json').then(contents => {
-      const languages = JSON.parse(contents)
-      const config = languages[language]
-      resolve(config)
-    })
-  })
-}
-
-const executeCode = (config, code) => {
+module.exports = function run (language, code) {
+  const config = languages[language];
+  if (typeof code !== 'string' || !config) return ''
   return new Promise((resolve, reject) => {
     const name = ulid.ulid()
-    const fileName = `${name}${config.extension}`
+    const fileName = `${name}.${config.extension}`
     fs.writeFile(fileName, code).then(() => {
       let output = Buffer.alloc(0)
-      const docker = cp.spawn('docker', buildArgs(config, name))
+      const docker = cp.spawn('docker', buildArgs(config, name, fileName))
       docker.stdout.on('data', (chunk) => { output = Buffer.concat([output, chunk]) })
       docker.stderr.on('data', (chunk) => { output = Buffer.concat([output, chunk]) })
 
@@ -47,13 +40,4 @@ const executeCode = (config, code) => {
       })
     })
   })
-}
-
-module.exports = function run (language, code) {
-  if (typeof code !== 'string') return ''
-  return parseLanguage(language)
-    .then(config => {
-      if (!config) return ''
-      return executeCode(config, code)
-    })
 }
