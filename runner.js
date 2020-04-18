600786e06f4108f13e78e7caa6ce744f02bbbaa8
korlando7 <korlando7@gmail.com>
2020-04-18 16:13:22 -0400

    add multi lang with python, add husky

--- runner.js
+++ runner.js
@@ -1,42 +1,59 @@
-const fs = require('fs')
+const fs = require('fs').promises
 const cp = require('child_process')
-const util = require('util')
 const ulid = require('ulid')
 
-const writeFile = util.promisify(fs.writeFile)
-const deleteFile = util.promisify(fs.unlink)
+const buildArgs = (config, name) => {
+  const { image, cmd, extension } = config
+  return [
+    'run',
+    '--rm',
+    '--name',
+    name,
+    '-v',
+    `${process.cwd()}:/usr/src/app`,
+    '-w',
+    '/usr/src/app',
+    image,
+    cmd,
+    `${name}${extension}`
+  ]
+}
 
-const buildArgs = (name) => [
-  'run',
-  '--rm',
-  '--name',
-  name,
-  '-v',
-  `${process.cwd()}:/usr/src/app`,
-  '-w',
-  '/usr/src/app',
-  'node:12-alpine',
-  'node',
-  `${name}.js`
-]
+const parseLanguage = (language) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile('./languages.json').then(contents => {
+      const languages = JSON.parse(contents)
+      const config = languages[language]
+      resolve(config)
+    })
+  })
+}
 
-module.exports = function run (language, code) {
-  if (typeof code !== 'string') return ''
+const executeCode = (config, code) => {
   return new Promise((resolve, reject) => {
     const name = ulid.ulid()
-    writeFile(`${name}.js`, code).then(() => {
+    const fileName = `${name}${config.extension}`
+    fs.writeFile(fileName, code).then(() => {
       let output = Buffer.alloc(0)
-
-      const docker = cp.spawn('docker', buildArgs(name))
+      const docker = cp.spawn('docker', buildArgs(config, name))
       docker.stdout.on('data', (chunk) => { output = Buffer.concat([output, chunk]) })
       docker.stderr.on('data', (chunk) => { output = Buffer.concat([output, chunk]) })
 
       docker.on('error', (err) => { reject(err) })
       docker.on('exit', () => {
-        deleteFile(`${name}.js`).then(() => {
+        fs.unlink(fileName).then(() => {
           resolve(output.toString())
         })
       })
     })
   })
+}
+
+module.exports = function run (language, code) {
+  if (typeof code !== 'string') return ''
+  return parseLanguage(language)
+    .then(config => {
+      if (!config) return ''
+      return executeCode(config, code)
+    })
 }
