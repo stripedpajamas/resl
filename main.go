9b267eac895a4a1b09c3c57a999981cf6e8e239b
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-12-01 22:23:26 -0800

    Proper output struct

--- main.go
+++ main.go
@@ -4,14 +4,21 @@ import (
         "fmt"
         "context"
         "github.com/aws/aws-lambda-go/lambda"
+        "github.com/aws/aws-lambda-go/events"
 )
 
 type MyEvent struct {
         Name string `json:"name"`
 }
 
-func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
-        return fmt.Sprintf("Hello %s!", name.Name ), nil
+func HandleRequest(ctx context.Context, name MyEvent) (events.APIGatewayProxyResponse, error) {
+        return events.APIGatewayProxyResponse {
+                Body: fmt.Sprintf("Hello %s!", name.Name ),
+                StatusCode: 200,
+                Headers: make(map[string]string),
+                MultiValueHeaders: make(map[string][]string),
+                IsBase64Encoded: false,
+        }, nil
 }
 
 func main() {
