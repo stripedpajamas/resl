8ca6acc4b6d3d97d5a8b734edc0847312c72c433
korlando7 <korlando7@gmail.com>
2020-12-02 22:00:58 -0800

    first try at lambda client

--- go.mod
+++ go.mod
@@ -4,6 +4,10 @@ go 1.15
 
 require (
 	github.com/aws/aws-lambda-go v1.20.0
+	github.com/aws/aws-sdk-go v1.36.0
 	github.com/kr/text v0.2.0 // indirect
+	golang.org/x/net v0.0.0-20201202161906-c7110b5ffcbb // indirect
+	golang.org/x/text v0.3.4 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	lambdaClient "github.com/aws/aws-sdk-go/service/lambda"
 )
+
+type code struct {
+	Text     string
+	Language string
+}
 
 func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	fmt.Printf("Processing request data for request %s.\n", request.RequestContext.RequestID)
@@ -14,6 +24,29 @@ func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 	fmt.Println("Headers:")
 	for key, value := range request.Headers {
 		fmt.Printf("    %s: %s\n", key, value)
+	}
+
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	client := lambdaClient.New(sess, &aws.Config{Region: aws.String("us-west-2")})
+
+	payload, err := json.Marshal(code{Text: "console.log('hello')", Language: "javascript"})
+	if err != nil {
+		fmt.Println(err)
+		return events.APIGatewayProxyResponse{
+			StatusCode: 400,
+		}, nil
+	}
+
+	_, err = client.Invoke(&lambdaClient.InvokeInput{FunctionName: aws.String("ReslLangLambda"), Payload: payload})
+
+	if err != nil {
+		fmt.Println(err)
+		return events.APIGatewayProxyResponse{
+			StatusCode: 400,
+		}, nil
 	}
 
 	return events.APIGatewayProxyResponse{
