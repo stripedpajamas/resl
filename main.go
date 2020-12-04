080cb0338acc84c5c7cd457e104b775180ca0b20
korlando7 <korlando7@gmail.com>
2020-12-03 16:10:44 -0800

    draft code to parse request body

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -12,18 +13,86 @@ import (
 	lambdaClient "github.com/aws/aws-sdk-go/service/lambda"
 )
 
-type code struct {
-	Text     string
-	Language string
+// RequestBody represents the model of the incoming request body
+type RequestBody struct {
+	TriggerID   int    `json:"trigger_id"`
+	Text        string `json:"text"`
+	ResponseURL string `json:"response_url"`
+}
+
+// CodePayload represents the request payload sent to the invoked language lambda ** MOVE TO SHARED PKG **
+type CodePayload struct {
+	Code     string `json:"code"`
+	Language string `json:"language"`
+}
+
+func processRequestBody(request events.APIGatewayProxyRequest) (RequestBody, error) {
+	fmt.Printf("Incoming request body: %s", request.Body)
+
+	var body RequestBody
+
+	if err := json.Unmarshal([]byte(request.Body), &body); err != nil {
+		fmt.Println(err)
+		return body, err
+	}
+
+	return body, nil
+}
+
+func getCodePayloadFromRequestBody(body RequestBody) ([]byte, error) {
+	var payload = parseText(body.Text)
+
+	return json.Marshal(payload)
+}
+
+func parseCode(payload *CodePayload) {
+	// TODO
+	// clean code string
+}
+
+func parseLanguage(payload *CodePayload) {
+	// TODO
+	// parse language
+	// validate that it is supported
+}
+
+func parseText(text string) CodePayload {
+	text = strings.Trim(text, " ")
+	chars := []rune(text)
+	separatorIdx := -1
+	currIdx := 0
+	textLen := len(chars)
+	code := ""
+	lang := text
+	var c rune
+
+	for separatorIdx == -1 && currIdx < textLen {
+		c = rune(chars[currIdx])
+
+		if c == ' ' {
+			separatorIdx = currIdx
+		}
+
+		currIdx++
+	}
+
+	if separatorIdx > -1 {
+		code = string(chars[separatorIdx+1 : textLen])
+		lang = string(chars[0:separatorIdx])
+	}
+
+	return CodePayload{
+		Code:     code,
+		Language: lang,
+	}
 }
 
 func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	fmt.Printf("Processing request data for request %s.\n", request.RequestContext.RequestID)
-	fmt.Printf("Body size = %d.\n", len(request.Body))
-
-	fmt.Println("Headers:")
-	for key, value := range request.Headers {
-		fmt.Printf("    %s: %s\n", key, value)
+	body, err := processRequestBody(request)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 400,
+		}, err
 	}
 
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
@@ -32,7 +101,7 @@ func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 
 	client := lambdaClient.New(sess, &aws.Config{Region: aws.String("us-west-2")})
 
-	payload, err := json.Marshal(code{Text: "console.log('hello')", Language: "javascript"})
+	payload, err := getCodePayloadFromRequestBody(body)
 
 	if err != nil {
 		fmt.Println(err)
