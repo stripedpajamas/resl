f5c113ed07a7a9ae1a6dcde72face56bd45c623c
korlando7 <korlando7@gmail.com>
2020-12-08 21:07:58 -0800

    clean up code runner

--- lambdas/code_exec/index.js
+++ lambdas/code_exec/index.js
@@ -1,64 +1,43 @@
-const cp = require('child_process')
 const fs = require('fs/promises')
 const path = require('path')
+const execa = require('execa')
+const ULID = require('ulid')
 
-exports.handler = async (event) => {
-  const { language, code } = event
-  
-  const fp = await writeCodeFile('index.js', code)
-  const out = await runCode(fp)
+exports.handler = async ({ code, props }) => {
+  console.log('Running Code Handler')
+
+  const { extension, runCmd } = props
+
+  const filePath = createFilePath("./", extension)
+  await writeCodeFile(filePath, code)
+  const out = await runCode(runCmd, [filePath])
+  await deleteCodeFile(filePath)
 
   return out
 }
 
-async function writeCodeFile(fileName, code) {
-	console.log("Writing Code File")
-
-	const filepath = path.join("/tmp", fileName)
-
-	console.log("Writing file: %s \n", filepath)
-
-  await fs.writeFile(filepath, code)
-
-	console.log("Created file to execute code: %s \n", filepath)
-
-	return filepath
+const createFilePath = (folder, extension) => {
+  const file = `${ULID.ulid()}.${extension}`
+  return path.join(folder, file)
 }
 
-async function runCode(filepath) {
-  console.log("Running Code")
+const writeCodeFile = async (filePath, code) => {
+	console.log(`Writing file: ${filePath}`)
 
-  return runAndCollectOutput('node', [filepath], {
-    timeout: 10000
-  })
+  await fs.writeFile(filePath, code)
+
+	console.log(`Created file to execute code: ${filePath}`)
 }
 
-function runAndCollectOutput (cmd, args, opts = {}) {
-  return new Promise((resolve, reject) => {
-    let output = Buffer.alloc(0)
-    const child = cp.spawn(cmd, args)
-
-    let timeout
-    if (opts.timeout) {
-      timeout = setTimeout(() => {
-        child.kill()
-        const error = new Error('Execution timed out')
-        error.code = 'TIMEOUT'
-        reject(error)
-      }, opts.timeout)
-    }
-
-    child.stdout.on('data', (chunk) => { output = Buffer.concat([output, chunk]) })
-    child.stderr.on('data', (chunk) => { output = Buffer.concat([output, chunk]) })
-
-    child.on('error', (e) => { // error event is emitted when child could not be spawned
-      clearTimeout(timeout)
-      reject(e)
-    })
-    child.on('exit', (code) => {
-      clearTimeout(timeout)
-      resolve({ output: output.toString(), code })
-    })
-  })
+const deleteCodeFile = async (filePath) => {
+  console.log(`Deleting file: ${filePath}`)
+  await fs.unlink(filePath)
 }
 
+const runCode = async (cmd, args) => {
+  console.log('Running Code')
+
+  const { all } = await execa(cmd, args, { all: true })
+
+  return all
+}
