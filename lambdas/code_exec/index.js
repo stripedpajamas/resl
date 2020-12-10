6d9d7dae5c618674bd5ea343d7aaa445aafbfe8d
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-12-09 16:44:01 -0800

    Add modal methods to Slack module

--- lambdas/code_exec/index.js
+++ lambdas/code_exec/index.js
@@ -43,3 +43,12 @@ const runCode = async (cmd, args) => {
 
   return output
 }
+
+exports.handler({
+	code: 'for (let i = 0; i < 5; i++) { console.log(i) }',
+	props: {
+		extension: 'js',
+		runCmd: 'node'
+	}
+}).then(console.log).catch(console.error)
+
